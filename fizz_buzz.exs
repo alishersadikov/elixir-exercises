6ff103a4d0ad4539ed6255484cee8672df8860d5
Alisher Sadikov <asadikov@msts.com>
2019-04-18 21:54:29 -0500

    Solve FizzBuzz without conditionals

--- fizz_buzz.exs
+++ fizz_buzz.exs
@@ -2,8 +2,23 @@ fizz_buzz = fn
   { 0, 0, _ } -> "FizzBuzz"
   { 0, _, _ } -> "Fizz"
   { _, 0, _ } -> "Buzz"
+  { _, _, c } -> c
 end
 
-IO.puts fizz_buzz.({ 0, 0, 1 })
-IO.puts fizz_buzz.({ 0, 1, 1 })
-IO.puts fizz_buzz.({ 1, 0, 1 })
+IO.puts fizz_buzz.({ 0, 0, 3 })
+IO.puts fizz_buzz.({ 0, 2, 3 })
+IO.puts fizz_buzz.({ 1, 0, 3 })
+IO.puts fizz_buzz.({ 1, 2, 3 })
+
+evaluate = fn
+  (n) -> evaluate.({rem(n, 3), rem(n, 5), n})
+end
+
+IO.puts evaluate.(10)
+IO.puts evaluate.(11)
+IO.puts evaluate.(12)
+IO.puts evaluate.(13)
+IO.puts evaluate.(14)
+IO.puts evaluate.(15)
+IO.puts evaluate.(16)
+
